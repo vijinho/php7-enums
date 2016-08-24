41e7e18990e3dfc239e089c568b246ecfc251b86
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-08-24 17:18:44 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Enums/Enum.php
+++ src/Enums/Enum.php
@@ -114,7 +114,7 @@ class Enum implements \Serializable
         if (empty($allowed)) {
             throw new \LogicException('Method not allowed.');
         }
-        $values =& static::$values;
+        $values = & static::$values;
         if (array_key_exists($key, $values)) {
             unset($values[$key]);
         } else {
@@ -135,14 +135,14 @@ class Enum implements \Serializable
     public static function fixKeys(array $values = [])
     {
         if (empty($values)) {
-            $values =& static::$values;
+            $values = & static::$values;
         }
 
         foreach ($values as $k => $v) {
             $oldKey = $k;
             if (!is_string($k)) {
                 if (!is_string($v)) {
-                    throw new \UnexpectedValueException(sprintf("Key '%s' for value '%s' is not a string!", print_r($k,1), print_r($v,1)));
+                    throw new \UnexpectedValueException(sprintf("Key '%s' for value '%s' is not a string!", print_r($k, 1), print_r($v, 1)));
                 }
                 // if the key is not a string, use the value if it is a string
                 $k = $v;
@@ -175,7 +175,7 @@ class Enum implements \Serializable
             throw new \LogicException('Overwrite not allowed.');
         }
 
-        $values =& static::$values;
+        $values = & static::$values;
 
         // if it's a string, convert to array
         if (is_string($newValues)) {
@@ -224,7 +224,7 @@ class Enum implements \Serializable
                     $caseSensitive = static::$caseSensitive;
                 }
                 if (empty($caseSensitive)) {
-                    $values = array_map(function($value){
+                    $values = array_map(function($value) {
                         return strtoupper($value);
                     }, $values);
                 }
@@ -232,13 +232,13 @@ class Enum implements \Serializable
             $keys = array_keys($values, $value);
             $count = count($keys);
             if (0 === $count) {
-                throw new \InvalidArgumentException(sprintf("Key for value '%s' does not exist.", print_r($value,1)));
+                throw new \InvalidArgumentException(sprintf("Key for value '%s' does not exist.", print_r($value, 1)));
             }
             return count($keys) > 1 ? $keys : $keys[0];
         } elseif (is_array($value)) {
             $search = array_search($value, $values);
             if (false === $search) {
-                throw new \InvalidArgumentException(sprintf("Key for value '%s' does not exist.", print_r($value,1)));
+                throw new \InvalidArgumentException(sprintf("Key for value '%s' does not exist.", print_r($value, 1)));
             }
             return $search;
         }
@@ -297,7 +297,7 @@ class Enum implements \Serializable
             $values = array_change_key_case($values, CASE_UPPER);
         }
         if (false === array_key_exists($key, $values)) {
-            throw new \InvalidArgumentException(sprintf("Value for key '%s' does not exist.", print_r($key,1)));
+            throw new \InvalidArgumentException(sprintf("Value for key '%s' does not exist.", print_r($key, 1)));
         }
         return $values[$key];
     }
@@ -353,8 +353,7 @@ class Enum implements \Serializable
     public function __isset($key)
     {
         return empty(static::$caseSensitive) ?
-            array_key_exists(strtoupper($key), array_change_key_case(static::$values, CASE_UPPER)) :
-            array_key_exists($key, static::$values);
+            array_key_exists(strtoupper($key), array_change_key_case(static::$values, CASE_UPPER)) : array_key_exists($key, static::$values);
     }
 
 
